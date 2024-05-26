a602086d2fe115f4e73f588a20854d1cc7d9a252
sekercirsin <irsinskrc@icloud.com>
2024-05-26 17:00:29 +0300

    2 images last

--- SlidingPuzzle.java
+++ SlidingPuzzle.java
@@ -92,8 +92,8 @@ public class SlidingPuzzle extends JFrame {
 
         add(puzzlePanel);
         setTitle("Sliding Puzzle");
-       // setSize(840, 840);
-       pack();
+        setSize(840, 840);
+       
         this.addWindowListener(new WindowListener() {
             @Override
             public void windowOpened(WindowEvent e) {
